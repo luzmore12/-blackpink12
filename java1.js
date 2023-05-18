5fd4757004dbf5cd0095506084e55a2b4ec74aab
luzmore12 <132724237+luzmore12@users.noreply.github.com>
2023-05-18 12:19:17 -0500

    Add files via upload

--- java1.js
+++ java1.js
@@ -1,1 +1,19 @@
-document.addEventListener()
+class leguaje {
+    constructor(nombre,año) {
+        this.nombre = nombre;
+        this.año = año;
+    }
+    descripcion() {
+        console.log("${this.nombre}) fue creado en ${this.año}");
+        
+    }
+}
+
+let javaScript = new leguaje("javaScript",2023)
+javaScript.descripcion();
+
+let html = new leguaje("HTML",2023)
+html.descripcion();
+
+let css = new leguaje("CSS",2023)
+css.descripcion();
